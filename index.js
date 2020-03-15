81aa3767dbab3203368dce2508b62c20bb45f0e0
thekingofjibs <allen-b@outlook.com>
2020-03-15 11:44:52 +0000

    websocket added

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
-var express = require("express");
+const express = require('express')
+var app = express()
+var http = require('http').createServer(app)
 var bodyParser = require("body-parser");
-var socket = require("socket.io")
-var app = express();
+var io = require("socket.io")(http);
+
+var name1 = ""
+var name2 = ""
+var score1 = 0
+var score2 = 0
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/third',function(req,res){
-  res.sendfile("socket_third.html");
-});
-
-app.get('/scoreboard',function(req,res){
-    res.sendfile("socket_scoreboard.html");
-});
-
 app.post('/score',function(req,res){
-  var score1 = req.body.score1;
-  var score2 = req.body.score2;
-  console.log("Score is: "+score1+":"+score2);
+  name1 = req.body.name1;
+  name2 = req.body.name2;
+  score1 = req.body.score1;
+  score2 = req.body.score2;
+  console.log("Score updated to: "+score1+"-"+score2);
+  io.emit('scoreUpdate', {"name1": name1, "score1": score1,"name2": name2, "score2":score2})
   res.end("done");
 });
 
 app.listen(8080,function(){
   console.log("Started on PORT 8080");
+})
+
+io.on('connection', function(socket){
+  console.log("template connected");
+  socket.on('getScore',function(args){
+    io.emit('scoreUpdate', {"name1": name1, "score1": score1,"name2": name2, "score2":score2})
+    console.log("score sent");
+  });
 })
